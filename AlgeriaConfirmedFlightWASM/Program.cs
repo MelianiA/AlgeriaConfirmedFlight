6a348b6af5283d7439c180873aa99d6756e1cadd
Gitlab CI <gitlab@kis.fr>
2022-03-03 16:26:17 +0100

    mod1626

--- AlgeriaConfirmedFlightWASM/Program.cs
+++ AlgeriaConfirmedFlightWASM/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddMudServices();
 builder.Services.AddScoped(sp =>
     new HttpClient
     {
-        BaseAddress = new Uri(@"https://localhost:5011/")
+        BaseAddress = new Uri(@"http://www.iasoft.fr/api/")
     });
 builder.Services.AddScoped<IVolConfirmeService, VolConfirmeService>();
  
