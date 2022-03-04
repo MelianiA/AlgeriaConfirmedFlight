9adfd8f42ea403f7ae7dce3a8477fcfd657db01c
Gitlab CI <gitlab@kis.fr>
2022-03-04 17:27:47 +0100

    17:27

--- AlgeriaConfirmedFlightWASM/Program.cs
+++ AlgeriaConfirmedFlightWASM/Program.cs
@@ -1,10 +1,16 @@
 using AlgeriaConfirmedFlightWASM;
 using AlgeriaConfirmedFlightWASM.Services;
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+// add after builder initialization:
+builder.Services.AddBlazoredLocalStorage();   // local storage
+builder.Services.AddBlazoredLocalStorage(config => config.JsonSerializerOptions.WriteIndented = true);  // local storage
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddMudServices();
---
+++ AlgeriaConfirmedFlightWASM/StaticCode/StaticsElements.cs
@@ -0,0 +1,14 @@
+﻿namespace AlgeriaConfirmedFlightWASM.StaticCode
+{
+    public class StaticsElements
+    {
+        public static string CodeSecret { get; set; } = "";
+
+        public static bool SecretValide()
+        {
+            return CodeSecret.Equals("abc",StringComparison.OrdinalIgnoreCase);
+        }
+
+
+    }
+}
